3d88e3dcde30e0e213df4f37b4145bbc085e0268
Assuliya <t.nesterchuk@mail.ru>
2016-09-15 17:58:53 +0300

    main functionality of the site is set

--- apps/blog/models.py
+++ apps/blog/models.py
@@ -1,5 +1,91 @@
 from __future__ import unicode_literals
-
+from django.core.exceptions import ObjectDoesNotExist
 from django.db import models
+import bcrypt, re
 
-# Create your models here.
+PASS_REGEX = re.compile(r'\d.*[A-Z]|[A-Z].*\d')
+
+class UserManager(models.Manager):
+    def validateReg(self, request):
+        errors = []
+        if len(request.POST['username']) < 1:
+            errors.append('Username can not be empty')
+
+        try:
+            user = User.objects.get(username = request.POST['username'])
+            errors.append('This username is already being used')
+        except ObjectDoesNotExist:
+            pass
+
+        if len(errors) > 0:
+            return (False, errors)
+        return (True, errors)
+
+    def validateRegPass(self, request):
+        errors = []
+        if len(request.POST['password']) < 1:
+            errors.append('Password can not be empty')
+        elif len(request.POST['password']) < 8:
+            errors.append('Password should be more than 7 characters')
+        elif not PASS_REGEX.match(request.POST['password']):
+            errors.append('Password should contain at least one apper case letter and one number')
+        if request.POST['password'] != request.POST['repeat']:
+            errors.append('Password repeat did not match the password')
+
+        if len(errors) > 0:
+            return (False, errors)
+        return (True, errors)
+
+    def validateLogin(self, request):
+        from bcrypt import hashpw, gensalt
+        errors = []
+        try:
+	        user = self.get(username=request.POST['username'])
+	        password = user.pw_hash.encode()
+	        loginpass = request.POST['password'].encode()
+	        print password
+	        print hashpw(loginpass, password)
+	        if hashpw(loginpass, password) == password:
+	            return (True, user)
+	        else:
+	            errors.append("Sorry, no password match. Please try again.")
+	            return (False, errors)
+        except ObjectDoesNotExist:
+            pass
+        errors.append("Sorry, no email found. Please try again.")
+        return (False, errors)
+
+
+class User(models.Model):
+      username = models.CharField(max_length=45)
+      image = models.ImageField(upload_to='blog/images/user/', default='blog/images/post/anonym.png')
+      about = models.TextField(default = "")
+      pw_hash = models.CharField(max_length=255)
+      created_at = models.DateTimeField(auto_now_add = True)
+      updated_at = models.DateTimeField(auto_now = True)
+
+      objects = models.Manager()
+      manager = UserManager()
+
+class Tag(models.Model):
+      name = models.CharField(max_length=45)
+      use = models.PositiveIntegerField(default = 0)
+      created_at = models.DateTimeField(auto_now_add=True)
+      updated_at = models.DateTimeField(auto_now=True)
+
+class Post(models.Model):
+      title = models.CharField(max_length=100)
+      post = models.TextField()
+      image = models.ImageField(upload_to='blog/images/post/', default='blog/images/post/no.png')
+      user_id = models.ForeignKey(User)
+      tags = models.ManyToManyField(Tag, related_name='post_tag')
+      like = models.PositiveIntegerField(default = 0)
+      created_at = models.DateTimeField(auto_now_add=True)
+      updated_at = models.DateTimeField(auto_now=True)
+
+class Comment(models.Model):
+      comment = models.TextField(max_length=1000)
+      user_id = models.ForeignKey(User)
+      post_id = models.ForeignKey(Post)
+      created_at = models.DateTimeField(auto_now_add=True)
+      updated_at = models.DateTimeField(auto_now=True)
--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,5 +1,120 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse
+from django.contrib import messages
+from models import User, Post, Comment,Tag
+import bcrypt
 
 def index(request):
     return render(request, 'blog/index.html')
+
+def login(request):
+    return render(request, 'blog/login.html')
+
+def register(request):
+    return render(request, 'blog/register.html')
+
+def show(request, user_id):
+    if not 'user' in request.session:
+        return redirect(reverse('index'))
+    user = User.manager.get(id=user_id)
+    posts = Post.objects.filter(user_id=user_id)
+    comments = Comment.objects.all()
+    context = {'user':user, 'posts':posts, 'comments': comments}
+    return render(request, 'blog/show.html', context)
+
+def edit(request, user_id):
+    user = User.objects.get(id = user_id)
+    context = {'user':user}
+    return render(request, 'blog/edit.html', context)
+
+def dashboard(request):
+    users = User.objects.all()
+    context = {'users':users}
+    return render(request, 'blog/dashboard.html', context)
+
+def deletion_page(request):
+    return render(request, 'blog/delete.html')
+
+def register_process(request):
+    result = User.manager.validateReg(request)
+    resultPass = User.manager.validateRegPass(request)
+    if result[0] == False or resultPass[0] == False:
+        errors = result[1]+resultPass[1]
+        print_messages(request, errors)
+        return redirect(reverse('register'))
+    pw_hash = bcrypt.hashpw(request.POST['password'].encode(), bcrypt.gensalt())
+    user = User.manager.create(username=request.POST['username'], pw_hash=pw_hash)
+    return log_user_in(request, user)
+
+def login_process(request):
+    result = User.manager.validateLogin(request)
+    if result[0] == False:
+        print_messages(request, result[1])
+        return redirect(reverse('login'))
+    return log_user_in(request, result[1])
+
+def print_messages(request, message_list):
+    for message in message_list:
+        messages.add_message(request, messages.ERROR, message)
+
+def log_user_in(request, user):
+    request.session['user'] = user.id
+    return redirect(reverse('show', kwargs={'user_id':request.session['user']}))
+
+def update(request):
+    user = User.manager.get(id=request.session['user'])
+    if user.username != request.POST['username']:
+        result = User.manager.validateReg(request)
+        if result[0] == False:
+            print_messages(request, result[1])
+            return redirect(reverse('edit', kwargs={'user_id':request.session['user']}))
+    update = User.objects.get(id = request.session['user'])
+    update.username = request.POST['username']
+    update.image = request.POST['image']
+    update.save(update_fields=None)
+    return redirect(reverse('show', kwargs={'user_id':request.session['user']}))
+
+def update_pass(request):
+    result = User.manager.validateRegPass(request)
+    result2 = User.manager.validateLogin(request)
+    if result[0] == False or result2[0] == False:
+        if result2[0] != False:
+            print_messages(request, result[1])
+        else:
+            print_messages(request, result2[1])
+        return redirect(reverse('edit', kwargs={'user_id':request.session['user']}))
+    pw_hash = bcrypt.hashpw(request.POST['password'].encode(), bcrypt.gensalt())
+    update = User.objects.get(id = request.session['user'])
+    update.pw_hash = pw_hash
+    update.save(update_fields=None)
+    return redirect(reverse('dashboard_show', kwargs={'user_id':request.session['user']}))
+
+def logout(request):
+    user = User.manager.get(id=request.session['user'])
+    user.user_level = 0
+    user.save(update_fields=None)
+    request.session.pop('user')
+    return redirect(reverse('index'))
+
+def delete(request):
+    User.objects.filter(id = request.session['user']).delete()
+    request.session.pop('user')
+    return redirect(reverse('index'))
+
+def add_post(request):
+    if request.method == "POST":
+        user = User.objects.get(id=request.session['user'])
+        Post.objects.create(title = request.POST['title'], message = request.POST['message'], user_id = user)
+        return redirect(reverse('show', kwargs={'user_id':request.session['user']}))
+    else:
+	    return redirect(reverse('show'))
+
+def add_comment(request, post_id):
+    if request.method == "POST":
+        user = User.objects.get(id=request.session['user'])
+        message = Message.objects.get(id=message_id)
+        page = message.user_id_to.id
+        Comment.objects.create(comment = request.POST['comment'], user_id = user, message_id = message)
+        return redirect(reverse('show', kwargs={'user_id':page}))
+    else:
+	    return redirect(reverse('show'))
